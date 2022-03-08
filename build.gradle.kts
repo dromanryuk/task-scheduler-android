b8a25db988479ac6608580ec148934a8edcaa385
dromanryuk <esed.pol@mail.ru>
2022-03-08 15:22:03 +0300

    Feat: migrate Gradle build scripts to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+val composeVersion by extra { "1.2.0-alpha04" }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
